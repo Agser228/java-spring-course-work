27ec3557eba98dd974f9e5365246e79472fe2bab
Alexey Klochek <klochek.a.m@edu.mirea.ru>
2022-04-06 23:55:23 +0300

    Add components to UI, remove extra files, update entities in model

--- server/src/main/java/ru/agser/server/controller/ChildController.java
+++ server/src/main/java/ru/agser/server/controller/ChildController.java
@@ -3,8 +3,8 @@ package ru.agser.server.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,16 +19,24 @@ import ru.agser.server.service.ChildService;
 import java.time.LocalDateTime;
 import java.util.Map;
 
-@Controller
-@RequestMapping("/child")
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/child")
 @RequiredArgsConstructor
-public class ChildController {
+public class ChildApiController {
     private final ChildService childService;
 
     @GetMapping("/list")
-    public String getChildren(Model model) {
-        model.addAttribute("children", childService.list(30));
-        return "children";
+    public ResponseEntity<Response> getChildren() {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .message("Children retrieved")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .data(Map.of("children", childService.list(30)))
+                        .build()
+        );
     }
 
     @GetMapping("/get/{id}")
@@ -39,7 +47,7 @@ public class ChildController {
                         .message("Child retrieved")
                         .status(HttpStatus.OK)
                         .statusCode(HttpStatus.OK.value())
-                        .data(Map.of("child", childService.get(id)))
+                        .data(Map.of("child", childService.getById(id)))
                         .build()
         );
     }
@@ -57,10 +65,17 @@ public class ChildController {
         );
     }
 
-    @GetMapping("/delete/{id}")
-    public String deleteChild(@PathVariable("id") Long id) {
-        childService.delete(id);
-        return "redirect:/child/list";
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Response> deleteChild(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .message("Child deleted")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .data(Map.of("deleted", childService.deleteById(id)))
+                        .build()
+        );
     }
 
 }
